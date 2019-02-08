d79c543afb8133d8337109a55945ffcae8503d6d
Maron martell Mraz <steklyanov@gmail.com>
2019-02-09 02:10:55 +0300

    initialized first app

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = 'test@test.ru'
+        password = 'Test123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'test@TEST.RU'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
