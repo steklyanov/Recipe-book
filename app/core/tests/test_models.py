3d740eeea3756f1523124559d7acfb31de49dfb2
Maron martell Mraz <steklyanov@gmail.com>
2019-02-09 17:36:46 +0300

    email validation and delete some trash

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -20,3 +20,17 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ test creating user with no email raises error """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_creating_new_superuser(self):
+        """ test creating new superuser """
+        user = get_user_model().objects.create_superuser(
+            'admin@admin.ru',
+            'admin'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
