8a82032f332940a30b602e91567d9fd260c9ba2b
steklyanov <steklyanov@gmail.com>
2019-03-17 18:20:42 +0300

    finish tag part
    Took 56 minutes

---
+++ app/recipe/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from app.core.models import Tag
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_field = ('id', )
