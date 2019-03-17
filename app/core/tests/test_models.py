1ab1fd862a683f3322b013fe0b17ae4fc078115b
steklyanov <steklyanov@gmail.com>
2019-03-17 14:51:46 +0300

    added tag model
    Took 1 minute

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from app.core import models
+
+
+def sample_user(email='test@test.ru', password='testpass'):
+    """ create sample user"""
+    return get_user_model().objects.create_user(email,password)
 
 
 class ModelTests(TestCase):
@@ -34,3 +40,11 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """ Test the tag string representation """
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Test'
+        )
+        self.assertEqual(str(tag), tag.name)
